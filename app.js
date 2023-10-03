ede76ad069ab4c0edb7984048e47741e6a06d004
AnthonyJPfau <anthony.pfau@gmail.com>
2023-10-03 15:48:48 -0400

    Zoom To state Function
    KEY_GOES_HERE must be replaced with google maps API key in order to function

--- app.js
+++ app.js
@@ -39,4 +39,32 @@ function loadGoogleMap() {
   
   // Call the function to load Google Maps when the page is loaded
   window.onload = loadGoogleMap;
+
+        // Replace 'YOUR_API_KEY' with your actual Google Maps API key
+        const apiKey = 'KEY_GOES_HERE';
+
+        let map;
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: { lat: 39.8283, lng: -98.5795 }, // Center of the United States
+                zoom: 4 // Initial zoom level
+            });
+        }
+
+        document.getElementById('zoomButton').addEventListener('click', () => {
+            const stateName = document.getElementById('stateInput').value;
+            
+            // Geocode the entered state name to get its coordinates
+            const geocoder = new google.maps.Geocoder();
+            geocoder.geocode({ address: `${stateName}, USA` }, (results, status) => {
+                if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
+                    const stateBounds = results[0].geometry.viewport;
+                    map.fitBounds(stateBounds);
+                } else {
+                    alert('State not found. Please enter a valid U.S. state name.');
+                }
+            });
+        });
+  
   
