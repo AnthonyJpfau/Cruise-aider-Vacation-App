5628e28b536ec1d7bdabfea2b2f40ed8975575a2
AnthonyJPfau <anthony.pfau@gmail.com>
2023-10-18 15:30:26 -0400

    Login POST works now

--- http_server.js
+++ http_server.js
@@ -4,9 +4,8 @@ const session = require('express-session');
 const axios = require('axios');
 const fs = require('fs');
 
-const JSONBIN_API_KEY = '$2a$10$P8UeB64FsrRlrItjngXAZO11qSMuLdiaZszplOqDntVoukdimnJ6a'; // Replace with your JSONbin.io API key
-const JSONBIN_BIN_ID = '653008f812a5d376598d6b8a'; // Replace with your JSONbin.io bin ID
-const JSONBIN_BASE_URL = `https://api.jsonbin.io/b/${JSONBIN_BIN_ID}`;
+const JSONBIN_API_KEY = '652ef0d09207ee75c9cb7053'; // Replace with your JSONbin.io API key 
+const JSONBIN_BASE_URL = `https://api.jsonbin.io/v3/b/653008f812a5d376598d6b8a`;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/'));
@@ -21,13 +20,13 @@ app.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
   try {
+    // Fetch user data from JSONbin.io
     const response = await axios.get(`${JSONBIN_BASE_URL}/latest`, {
       headers: {
         'secret-key': JSONBIN_API_KEY,
       },
     });
     const data = response.data;
-
     console.log('Fetched data from JSONbin:', data);
 
     // Check if user input is received correctly
@@ -35,27 +34,30 @@ app.post('/login', async (req, res) => {
     console.log('Received password:', password);
 
     // Find the user in the data
-    const user = data.users.find((user) => user.username === username && user.password === password);
+    const user = data.record.users.find((user) => user.username === username && user.password === password);
 
     if (user) {
+      // User exists, store the username in the session
       req.session.username = username;
       console.log('Login successful');
       res.redirect('/mapPage.html');
     } else {
+      // User doesn't exist, send an error message
       console.log('Login failed');
-      res.status(401).send('Incorrect username or password');
+      res.status(401).send('User does not exist or incorrect username or password');
     }
   } catch (error) {
-    console.error('Error connecting to JSONbin.io:', error);
-  
+    // Handle errors
     if (error.response) {
       console.error('Response status:', error.response.status);
       console.error('Response data:', error.response.data);
+    } else {
+      console.error('Request error:', error.message);
     }
-  
     res.status(500).send('Internal server error');
   }
 });
+
 
 // Serve static files
 app.get('/', (req, res) => {
