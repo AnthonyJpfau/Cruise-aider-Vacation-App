11a6e6fe9cff41053097eb59dfc5be6b1aea5692
AnthonyJPfau <anthony.pfau@gmail.com>
2023-11-01 13:52:48 -0400

    Clicking On map will now provide Coordinates + temporary marker

--- app.js
+++ app.js
@@ -49,52 +49,72 @@ document.addEventListener('DOMContentLoaded', () => {
   
 // Create a function to load the Google Maps code
 function loadGoogleMap() {
-    // Create a <script> element for Google Maps API
-    var script = document.createElement('script');
-    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDueM6P1BJmPhKC2Cp5jl6ufGPHMA4XC9o&callback=initMap';
-    script.async = true;
-    script.defer = true;
-  
-    // Define the callback function to initialize the map
-    script.onload = function () {
+  // Create a <script> element for Google Maps API
+  var script = document.createElement('script');
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDueM6P1BJmPhKC2Cp5jl6ufGPHMA4XC9o&callback=initMap';
+  script.async = true;
+  script.defer = true;
+
+  // Define the callback function to initialize the map
+  script.onload = function() {
       initMap();
-    };
-  
-    // Append the <script> element to the document's <head>
-    document.head.appendChild(script);
-  }
-  
-  // Create a function to initialize the map
+  };
 
-  
-  // Call the function to load Google Maps when the page is loaded
-  window.onload = loadGoogleMap;
+  // Append the <script> element to the document's <head>
+  document.head.appendChild(script);
+}
 
-        // Replace 'YOUR_API_KEY' with your actual Google Maps API key
-        const apiKey = 'KAIzaSyDueM6P1BJmPhKC2Cp5jl6ufGPHMA4XC9o';
+// Call the function to load Google Maps when the page is loaded
+window.onload = loadGoogleMap;
 
-        let map;
+// Replace 'YOUR_API_KEY' with your actual Google Maps API key
+const apiKey = 'KAIzaSyDueM6P1BJmPhKC2Cp5jl6ufGPHMA4XC9o';
 
-        function initMap() {
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: { lat: 39.8283, lng: -98.5795 }, // Center of the United States
-                zoom: 4 // Initial zoom level
-            });
+let map;
+let currentMarker; // This will store the current marker if one exists
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 39.8283, lng: -98.5795 }, // Center of the United States
+        zoom: 4 // Initial zoom level
+    });
+
+    // Event listener for map click to show coordinates and place a temporary marker
+    map.addListener('click', function(event) {
+        const clickedLat = event.latLng.lat();
+        const clickedLng = event.latLng.lng();
+
+        // Remove the previous marker if it exists
+        if (currentMarker) {
+            currentMarker.setMap(null);
         }
 
-        document.getElementById('zoomButton').addEventListener('click', () => {
-            const stateName = document.getElementById('stateInput').value;
-            
-            // Geocode the entered state name to get its coordinates
-            const geocoder = new google.maps.Geocoder();
-            geocoder.geocode({ address: `${stateName}, USA` }, (results, status) => {
-                if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
-                    const stateBounds = results[0].geometry.viewport;
-                    map.fitBounds(stateBounds);
-                } else {
-                    alert('State not found. Please enter a valid U.S. state name.');
-                }
-            });
+        // Place a new marker at the clicked location
+        currentMarker = new google.maps.Marker({
+            position: event.latLng,
+            map: map
         });
+
+        setTimeout(function() {
+          alert('You clicked at: ' + clickedLat + ', ' + clickedLng);
+      }, 50);  // Delay of 50 milliseconds
+  });
+}
+
+document.getElementById('zoomButton').addEventListener('click', () => {
+  const stateName = document.getElementById('stateInput').value;
+
+  // Geocode the entered state name to get its coordinates
+  const geocoder = new google.maps.Geocoder();
+  geocoder.geocode({ address: `${stateName}, USA` }, (results, status) => {
+      if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
+          const stateBounds = results[0].geometry.viewport;
+          map.fitBounds(stateBounds);
+      } else {
+          alert('State not found. Please enter a valid U.S. state name.');
+      }
+  });
+});
+
   
   
